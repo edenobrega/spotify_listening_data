c618b9f7044ef391314e698dad6adda4620cc3a3
nobs <edenobrega@sky.com>
2024-01-23 22:43:48 +0000

    initial

---
+++ Models/API/FeaturesResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spotifyloader.Models.API
+{
+    internal class FeaturesResponse
+    {
+        public List<FeatureResponse> audio_features { get; set; }
+    }
+}
